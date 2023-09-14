61f1ca2dc105983c3f98cd38e9d24fb21b4a82bf
HridaySarder <hridoyaug12@gmail.com>
2023-09-14 21:35:16 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-5xl text-center font-bold">
+      Course Registration
+    </h1>
+    </>
+  )
+}
+
+export default App
