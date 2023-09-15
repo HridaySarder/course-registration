9971b7637cff290c8e13f0b3cbcb225c03ee7393
HridaySarder <hridoyaug12@gmail.com>
2023-09-15 11:04:24 +0600

    credit hour and course name added

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -4,14 +4,16 @@
 const Course = ({selectedCourses}) => {
   console.log(selectedCourses);
   return (
-    <div>
-      <h2 className="text-4xl">Course Name:</h2>
+    <div className="my-5 bg-base-100 shadow-xl p-7 rounded-lg">
+      <h3 className="text-[#2F80ED] font-bold border-b-2 mb-5">Credit Hour Remaining</h3>
+      <h3 className="font-bold mb-4">Course Name:</h3>
       {
         selectedCourses.map((course) => (
           <p>{course.course_name}</p>
         )
         )
       }
+      <p className="border-t-2 mt-5">Total Credit Hour:</p>
     </div>
   );
 };
