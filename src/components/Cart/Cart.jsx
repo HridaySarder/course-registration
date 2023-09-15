a7f6e86a7d85877536df300115d36b37ce89cf03
HridaySarder <hridoyaug12@gmail.com>
2023-09-15 12:23:57 +0600

    added remaining credit hour and total credit hour functionality

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,45 +5,63 @@ import Course from "../Course/Course";
 
 const Cart = () => {
   const [allCoures, setAllCourse] = useState([]);
-  const [selectedCourses, setSelectedCourses] = useState([])
+  const [selectedCourses, setSelectedCourses] = useState([]);
+  const [remaining,setRemaining] = useState(0)
+  const [totalCredit,setTotalCredit] = useState(0)
   useEffect(() => {
     fetch("./course.json")
       .then((res) => res.json())
       .then((data) => setAllCourse(data));
   }, []);
-const handleSelectCourse = (course) =>{
-  setSelectedCourses([...selectedCourses,course])
+  const handleSelectCourse = (course) => {
+    const isExit = selectedCourses.find((item) => item.id == course.id);
+    let count = course.course_credit;
+    if (isExit) {
+      return alert("already booked");
+    } else {
+      selectedCourses.forEach((item) => {
+        count = count + item.course_credit;
+      });
+      const totalRemaining = 20-count;
+      if(count>20){
+        return alert('credit ses')
+      }
+      setTotalCredit(count)
+      setRemaining(totalRemaining)
+      setSelectedCourses([...selectedCourses, course]);
+    }
+  };
 
-}
-
-  console.log(selectedCourses);
+  
 
   return (
     <div className="flex gap-5">
       <div className="grid grid-cols-3 gap-5">
-        {
-          allCoures.map((course)=> (
-            <div className="card w-96 bg-base-100 shadow-xl">
-  <figure className="px-10 pt-10">
-    <img src={course.course_img} alt="Shoes" className="rounded-xl" />
-  </figure>
-  <div className="card-body items-center text-center">
-    <h2 className="card-title">{course.course_name}</h2>
-    <p>{course.course_details}</p>
-    <div className="flex justify-between gap-5">
-      <p>$ Price: {course.course_prize}</p>
-      <p>Credit:{course.course_credit}hr</p>
-    </div>
-    
-      <button onClick={() => handleSelectCourse(course)} className="btn w-full rounded-lg text-white bg-[#2F80ED]">Select</button>
-    
-  </div>
-</div>
-          ))
-        }
+        {allCoures.map((course) => (
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+              <img src={course.course_img} alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">{course.course_name}</h2>
+              <p>{course.course_details}</p>
+              <div className="flex justify-between gap-5">
+                <p>$ Price: {course.course_prize}</p>
+                <p>Credit:{course.course_credit}hr</p>
+              </div>
+
+              <button
+                onClick={() => handleSelectCourse(course)}
+                className="btn w-full rounded-lg text-white bg-[#2F80ED]"
+              >
+                Select
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
       <div>
-        <Course selectedCourses={selectedCourses}></Course>
+        <Course selectedCourses={selectedCourses} remaining = {remaining} totalCredit={totalCredit}></Course>
       </div>
     </div>
   );
--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/jsx-key */
 
 
-const Course = ({selectedCourses}) => {
+const Course = ({selectedCourses, remaining, totalCredit}) => {
   console.log(selectedCourses);
   return (
     <div className="my-5 bg-base-100 shadow-xl p-7 rounded-lg">
-      <h3 className="text-[#2F80ED] font-bold border-b-2 mb-5">Credit Hour Remaining</h3>
+      <h3 className="text-[#2F80ED] font-bold border-b-2 mb-5">Credit Hour Remaining {remaining} hr</h3>
       <h3 className="font-bold mb-4">Course Name:</h3>
       {
         selectedCourses.map((course) => (
@@ -13,7 +13,7 @@ const Course = ({selectedCourses}) => {
         )
         )
       }
-      <p className="border-t-2 mt-5">Total Credit Hour:</p>
+      <p className="border-t-2 mt-5">Total Credit Hour: {totalCredit}</p>
     </div>
   );
 };
