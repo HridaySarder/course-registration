e289d8b72e2451a035f4c8d0eadf44b114aa93b8
HridaySarder <hridoyaug12@gmail.com>
2023-09-16 11:28:19 +0600

    text and background color added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,16 @@
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable react/jsx-key */
 import { useState } from "react";
 import { useEffect } from "react";
 import Course from "../Course/Course";
-import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Cart = () => {
   const [allCoures, setAllCourse] = useState([]);
   const [selectedCourses, setSelectedCourses] = useState([]);
-  const [remaining,setRemaining] = useState(20)
-  const [totalCredit,setTotalCredit] = useState(0)
+  const [remaining, setRemaining] = useState(20);
+  const [totalCredit, setTotalCredit] = useState(0);
   useEffect(() => {
     fetch("./course.json")
       .then((res) => res.json())
@@ -24,21 +25,19 @@ const Cart = () => {
       selectedCourses.forEach((item) => {
         count = count + item.course_credit;
       });
-      const totalRemaining = 20-count;
-      if(count>20){
-        return toast('No credit available')
+      const totalRemaining = 20 - count;
+      if (count > 20) {
+        return toast("No credit available");
       }
-      setTotalCredit(count)
-      setRemaining(totalRemaining)
+      setTotalCredit(count);
+      setRemaining(totalRemaining);
       setSelectedCourses([...selectedCourses, course]);
     }
   };
 
-  
-
   return (
-    <div className="flex gap-5 mx-auto">
-      <div className="grid grid-cols-3 gap-3 ml-14 w-2/3">
+    <div className="flex gap-5 mx-auto pb-10">
+      <div className="grid grid-cols-3 gap-5 ml-16 ">
         {allCoures.map((course) => (
           <div className="card w-80 bg-base-100  shadow-xl">
             <figure className="px-10 pt-10">
@@ -46,14 +45,30 @@ const Cart = () => {
             </figure>
             <div className="card-body items-center text-center">
               <h2 className="card-title">{course.course_name}</h2>
-              <p>{course.course_details}</p>
+              <p className="text-[#1C1B1B99]">{course.course_details}</p>
               <div className="flex justify-between gap-5">
-                <p>$ Price: {course.course_prize}</p>
+                <p className="text-[#1C1B1B99]">
+                  $ Price: {course.course_prize}
+                </p>
                 <div className="flex gap-2">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-  <path d="M12 6.042C10.3516 4.56336 8.2144 3.74694 6 3.75C4.948 3.75 3.938 3.93 3 4.262V18.512C3.96362 18.172 4.97816 17.9989 6 18C8.305 18 10.408 18.867 12 20.292M12 6.042C13.6483 4.56328 15.7856 3.74685 18 3.75C19.052 3.75 20.062 3.93 21 4.262V18.512C20.0364 18.172 19.0218 17.9989 18 18C15.7856 17.9969 13.6484 18.8134 12 20.292M12 6.042V20.292" stroke="#1C1B1B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-                <p>Credit:{course.course_credit}hr</p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                  >
+                    <path
+                      d="M12 6.042C10.3516 4.56336 8.2144 3.74694 6 3.75C4.948 3.75 3.938 3.93 3 4.262V18.512C3.96362 18.172 4.97816 17.9989 6 18C8.305 18 10.408 18.867 12 20.292M12 6.042C13.6483 4.56328 15.7856 3.74685 18 3.75C19.052 3.75 20.062 3.93 21 4.262V18.512C20.0364 18.172 19.0218 17.9989 18 18C15.7856 17.9969 13.6484 18.8134 12 20.292M12 6.042V20.292"
+                      stroke="#1C1B1B"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                  <p className="text-[#1C1B1B99]">
+                    Credit: {course.course_credit}hr
+                  </p>
                 </div>
               </div>
 
@@ -69,7 +84,11 @@ const Cart = () => {
         ))}
       </div>
       <div>
-        <Course selectedCourses={selectedCourses} remaining = {remaining} totalCredit={totalCredit}></Course>
+        <Course
+          selectedCourses={selectedCourses}
+          remaining={remaining}
+          totalCredit={totalCredit}
+        ></Course>
       </div>
     </div>
   );
--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,18 +1,19 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-key */
 
-
-const Course = ({selectedCourses, remaining, totalCredit}) => {
+const Course = ({ selectedCourses, remaining, totalCredit }) => {
   console.log(selectedCourses);
   return (
     <div className="my-5 bg-base-100 shadow-xl p-7 rounded-lg w-80">
-      <h3 className="text-[#2F80ED] font-bold border-b-2 mb-5">Credit Hour Remaining {remaining} hr</h3>
+      <h3 className="text-[#2F80ED] font-bold border-b-2 mb-5">
+        Credit Hour Remaining {remaining} hr
+      </h3>
       <h3 className="font-bold mb-4">Course Name:</h3>
-      {
-        selectedCourses.map((course) => (
-          <li type="1">{course.course_name}</li>
-        )
-        )
-      }
+      {selectedCourses.map((course) => (
+        <li className="text-[#1C1B1B99]" type="1">
+          {course.course_name}
+        </li>
+      ))}
       <p className="border-t-2 mt-5">Total Credit Hour: {totalCredit}</p>
     </div>
   );
